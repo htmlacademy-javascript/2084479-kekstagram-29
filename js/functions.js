3a1cb9bb9cd62c16d167b348b3a8a9a9b6ea6dee
Юлия Мерзлякова <merzlyakova.yulya@gmail.com>
2023-06-20 10:59:30 +0500

    Генерация данных

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,6 @@
 // Функция для проверки длины строки
 const checkLength = (string, length) => string.length <= length;
 
-console.log(checkLength('проверяемая строка', 10));
-
 // Функция для проверки, является ли строка палиндромом
 const isPalindrome = (string) => {
   let editedStr = string.replaceAll(' ', '').toLowerCase();
@@ -12,8 +10,6 @@ const isPalindrome = (string) => {
   }
   return (editedStr === newStr);
 }
-
-console.log(isPalindrome('Кекс '));
 
 // Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа
 const returnIntegers = (str) => {
@@ -27,11 +23,3 @@ const returnIntegers = (str) => {
 
   return parseInt(newString.join(''), 10);
 }
-
-console.log(returnIntegers('2023 год'));
-console.log(returnIntegers('ECMAScript 2022'));
-console.log(returnIntegers('1 кефир, 0.5 батона'));
-console.log(returnIntegers('агент 007'));
-console.log(returnIntegers(2013));
-console.log(returnIntegers(-1));
-console.log(returnIntegers(1.5));
